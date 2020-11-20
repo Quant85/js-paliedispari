eabfa941d3b4408e43ae677933e37a681ad059c4
Quant85 <antonio.85.q@gmail.com>
2020-11-20 14:30:36 +0100

    Inizializzazione checker - PalindromeWord
    Mediante un promt chiedo all'utente una parola  da verifica, mediante la creazione della funzione palindormaChecker(userWord) genero un confronto specchiato dei caratteri che formano la stringa e dove la condizione è verificata faccio partire un contatore che "segnerà" quanti caratteri hanno avuto esito positivo alla verifica. Se al termine del ciclo for il parametro check avrà lo stesso valore del numero di char componenti la parola inserita, la funzione restituirà il valore booleano true altrimenti false. 
    Con il risultato di tale funzione do un condizionale, la dove la funciont palindormaChecker(userWord) sia true, apparirà un aler confermante che la verifica ha dato esito positivo, altrimenti un'alert avviserà l'utente che la verifica non è andata a buon fine e la parola inserita non è palindroma

---
+++ Palindroma/assets/js/main.js
@@ -0,0 +1,42 @@
+/* Palindroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
+//1. Chiedere all’utente di inserire una parola
+
+var userWord = prompt("Inserisci la parola che vuoi verificare sia palindroma");
+
+//2. Creare una funzione per capire se la parola inserita è palindroma
+
+function palindormaChecker(palindromaWord) {
+  var starChartWord, lastCharWord, check = 0;
+
+  for (var i = 0; i < palindromaWord.length; i++) {
+
+    //var starChartWord = userWord.charAt(i), lastCharWord = userWord.charAt((palindromaWord.length - i)), check, sumCheck;    
+
+    starChartWord = userWord.charAt(i);
+    console.log(starChartWord);
+    lastCharWord = userWord.charAt((palindromaWord.length - i - 1));
+    console.log(lastCharWord);
+
+
+    if (starChartWord === lastCharWord) {
+      check +=  1;
+      console.log(check);
+    } 
+    
+  }
+    if (check == palindromaWord.length ) {
+      return true;
+    } else {
+      return false;
+    }
+
+}
+
+if (palindormaChecker(userWord)) {
+  alert("Questa parola è palindroma");
+} else {
+  alert("Questa parola non è palindroma");
+}
